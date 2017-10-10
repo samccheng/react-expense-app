f229e0f9b60491c354b628b24257737ef1cda09a
sam <samcheng8@gmail.com>
2017-10-09 18:38:59 -0600

    setup webpack prod

--- src/app.js
+++ src/app.js
@@ -11,20 +11,6 @@ import './styles/styles.scss'
 
 const store = configureStore()
 
-
-store.dispatch(addExpense({ description: 'water bill', amount: 4500}))
-
-store.dispatch(addExpense({ description: 'gas bill', createdAt: 1000}))
-
-store.dispatch(addExpense({ description: 'rent', amount: 100000}))
-
-// store.dispatch(setTextFilter('water'))
-
-// const state = store.getState()
-// const visibleExpenses = getVisibleExpenses(state.expenses, state.filters)
-//
-// console.log(visibleExpenses)
-
 const jsx = (
   <Provider store={store}><AppRouter /></Provider>
 )
