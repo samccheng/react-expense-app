cbbbfb21c08c738a853e26eb89e1bf749b6b7b1d
sam <samcheng8@gmail.com>
2017-10-12 23:51:44 -0600

    setup test database

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,12 +1,12 @@
 import * as firebase from 'firebase'
 
 const config = {
-  apiKey: "AIzaSyA4lhtnmgQqF6rNXQXAC9hSAFNfSAKyjTQ",
-  authDomain: "expense-app-7014a.firebaseapp.com",
-  databaseURL: "https://expense-app-7014a.firebaseio.com",
-  projectId: "expense-app-7014a",
-  storageBucket: "expense-app-7014a.appspot.com",
-  messagingSenderId: "791213651057"
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.FIREBASE_DATABASE_URL,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID
 };
 
 firebase.initializeApp(config);
--- src/tests/setupTests.js
+++ src/tests/setupTests.js
@@ -1,5 +1,8 @@
+import Dotenv from 'dotenv'
 import Enzyme from 'enzyme'
 import Adapter from 'enzyme-adapter-react-16'
+
+Dotenv.config({ path: '.env.test' })
 
 Enzyme.configure({
   adapter: new Adapter()
