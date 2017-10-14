9c69feae1f33b494bf807cedb516880485dbf30e
sam <samcheng8@gmail.com>
2017-10-13 19:05:12 -0600

    fixed env

--- src/tests/setupTests.js
+++ src/tests/setupTests.js
@@ -1,9 +1,6 @@
-
-// require('dotenv').config({ path: '.env.test '})
 import Enzyme from 'enzyme'
 import Adapter from 'enzyme-adapter-react-16'
 import Dotenv from 'dotenv'
-
 
 Enzyme.configure({
   adapter: new Adapter()
